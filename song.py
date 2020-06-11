43704aaa45c802935254fb044db09378b663a2ac
Ayush Dangwal <dhairyad03@gmail.com>
2020-06-11 20:12:36 +0530

    Updated
    Updated Tags and optimized

--- song.py
+++ song.py
@@ -1,38 +1,27 @@
 from bs4 import BeautifulSoup as soup
 import urllib.request
 import urllib
-import os
+import webbrowser
 songn=input("Enter song's name       ")
-url="http://www.instamp3.net/download/"+songn.replace(" ","-")+".html"
+url="https://www.youtube.com/results?search_query="+songn.replace(" ","+")
 req = urllib.request.Request(
-    url, 
-    data=None, 
+    url,
+    data=None,
     headers={
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
     }
 )
-g = urllib.request.urlopen(req)
-pageh=g.read()
-pagehtml=soup(pageh,"html.parser")
-tabl=pagehtml.findAll("ul",{"class":"list-group"})
-table=tabl[0]
-metatags=table.findAll("div",{"class":"meta"})
-firstmeta=metatags[0]
-atag=firstmeta.findAll("a",{"class":"downnow"})
-ataga=atag[0]
-str=ataga["onclick"].split("'")
-url=str[1]
+uopen = urllib.request.urlopen(req)
+page=soup(uopen.read(),"html.parser")
+url2="https://320youtube.com"+page.findAll("div",{"class":"yt-lockup-content"})[0].findAll("a")[0]["href"]
 req = urllib.request.Request(
-    url, 
-    data=None, 
+    url2,
+    data=None,
     headers={
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
     }
-    )
-f = urllib.request.urlopen(req)
-downpage=f.read()
-pagen=soup(downpage,"html.parser")
-downbutt=pagen.findAll("a",{"class":"btn btn-danger"})
-cmd='cd C:\Program Files (x86)\Internet Download Manager\ && IDMan.exe /n /d '+'"'+downbutt[0]["href"]+'"'
-
-os.system(cmd)
+)
+uopen2 = urllib.request.urlopen(req)
+page2=soup(uopen2.read(),"html.parser")
+finallink=page2.findAll("div",{"id":"download"})[0].findAll("a")[0]["href"]
+webbrowser.open(finallink)
